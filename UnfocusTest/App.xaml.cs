730cebde61912dadb9f4c46a2c873bd3c8f4b70d
Tom Sproet <t.sproet@trnsprnt.nl>
2023-10-02 10:07:00 +0200

    mainpage stuff

--- UnfocusTest/App.xaml.cs
+++ UnfocusTest/App.xaml.cs
@@ -8,5 +8,14 @@
 
             MainPage = new AppShell();
         }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            LogoutUser();
+        }
+        public static void LogoutUser()
+        {
+            App.Current.MainPage = new AppShell();
+        }
     }
 }
