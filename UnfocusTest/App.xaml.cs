4a1101a9d833e6b71b0c6bb1c15972ec0db6e195
Tom Sproet <t.sproet@trnsprnt.nl>
2023-10-25 15:54:16 +0200

    Multiple tests for onresume

--- UnfocusTest/App.xaml.cs
+++ UnfocusTest/App.xaml.cs
@@ -11,8 +11,22 @@
         protected override void OnResume()
         {
             base.OnResume();
-            MainPage = new AppShell();
+
+            //Variant one
+            // MainPage = new AppShell();
+
+            //Variant two
+            MainPage = new NavigationPage(new OnResumePlaceholderPage());
+
+            
+            //Variant Three
+            //MainThread.BeginInvokeOnMainThread(async () =>
+            //{
+            //    //Uncomment to make variant three work
+            //    //await Task.Delay(100);
+            //    MainPage = new NavigationPage(new OnResumePlaceholderPage());
+            //});
         }
-       
+
     }
 }
